8651359250dee0c8abdfcb6bf41d33e31e68f4db
Santeri <santeri.j.kaariainen@gmail.com>
2017-05-11 23:45:15 +0300

    pug basics now work

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ var express = require('express');
 var https = require('https');
 var http = require('http');
 var app = express();
+app.set("view engine", "pug");
+app.set('views', path.join(__dirname, '/lib/views'));
 
 //http.createServer(app).listen(8080);
 https.createServer(options, app).listen(1443);
 var router = require(__dirname + "/lib/routing/router");
-router.init(app);
+app.get("/", function (req, res) {
+	res.render("test.pug", {user: "admin"});
+});
+//router.init(app);
 

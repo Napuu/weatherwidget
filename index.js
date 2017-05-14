e08e062fe4b8490058c11daaaa599b90125a514a
Santeri <santeri.j.kaariainen@gmail.com>
2017-05-14 22:06:18 +0300

    443 ei jostain syystä toimi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ fs = require('fs');
 var path = require("path");
 global.appRoot = path.resolve(__dirname);
 options = {
+	//openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.cert -days 365 -nodes
 	key: fs.readFileSync('./keys/server.key'),
 	cert: fs.readFileSync('./keys/server.crt')
 } 
@@ -12,11 +13,12 @@ var app = express();
 app.set("view engine", "pug");
 app.set('views', path.join(__dirname, '/lib/views'));
 
-//http.createServer(app).listen(8080);
+http.createServer(app).listen(8080);
 https.createServer(options, app).listen(1443);
 var router = require(__dirname + "/lib/routing/router");
 app.get("/", function (req, res) {
 	res.render("test.pug", {user: "admin"});
 });
-//router.init(app);
-
+router.init(app);
+var ip = require("ip");
+console.dir ( ip.address() );
